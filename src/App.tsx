4324c5c2ae4b76c9164ec6dd863c5c9c41348e1f
Michael Taggart <mtaggarthawk@gmail.com>
2022-04-29 18:34:47 -0600

    feat: typescript setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
-import { useState } from 'react';
 import './App.css';
 import { SetupSteps } from './components/SetupSteps';
 
-interface Process {
-  number: string;
-  title: string;
-  description: string;
-}
 
 function App() {
-  const process: Process[] = [
-    {
-      number: '01',
-      title: 'FILL OUT A PROFILE',
-      description: `We only want you to get games and gear that you'll love, so we'll ask for your preferences up front`,
-    },
-    {
-      number: '02',
-      title: 'REQUEST A DELIVERY',
-      description: `Once you're ready for your first delivery, all it takes is a click to get your shipment on the way.`,
-    },
-    {
-      number: '03',
-      title: 'KEEP WHAT YOU WANT',
-      description: `Tell us "no thanks" by returning any unwanted products in the enclosed packaging.`,
-    },
-    {
-      number: '04',
-      title: 'REQUEST ANOTHER DELIVERY',
-      description: `Get your next gaming fix by updating your profile then initiating your next shipment.`,
-    },
-  ];
-
   return (
     <div className="App">
       <header>
